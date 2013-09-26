a37311f52ae60c22c74c3e0c965337bf5d9418fc
sujino <ssujino@gmail.com>
2013-09-26 16:50:20 +0900

    add a comment to app.js

--- Resources/app.js
+++ Resources/app.js
@@ -2,7 +2,7 @@
 //
 // create base UI tab and root window
 //
-//require('/CommonNavigationWindow').createCommonNavigationWindow().open();
-require('/CreateNewMangaWindow').CreateNewMangaWindow().open();
+require('/CommonNavigationWindow').createCommonNavigationWindow().open();
+//require('/CreateNewMangaWindow').CreateNewMangaWindow().open();
 //require('/likesWindow').LikesWindow().open();
 
